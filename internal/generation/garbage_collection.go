1710b3e7c3f0af8038cf124bfad1cf56e30d0d5a
Inspirate789 <andreysapozhkov535@gmail.com>
2024-03-26 23:25:31 +0300

    Move mark worker to own file

--- internal/generation/garbage_collection.go
+++ internal/generation/garbage_collection.go
@@ -2,7 +2,6 @@ package generation
 
 import (
 	"math/rand"
-	"reflect"
 	"runtime"
 	"unsafe"
 )
@@ -23,77 +22,6 @@ func (gen *Generation) searchMetadata(addr unsafe.Pointer) (metadata *objectMeta
 
 func (gen *Generation) SearchObjectFunc() func(addr unsafe.Pointer) (metadata *objectMetadata, exist bool) {
 	return gen.searchMetadata
-}
-
-type markWorker struct {
-	id             int
-	gcID           uint64
-	visited        map[unsafe.Pointer]bool
-	searchMetadata func(addr unsafe.Pointer) (metadata *objectMetadata, exist bool)
-}
-
-func (mw markWorker) markObject(object *objectMetadata) (skip bool) {
-	if object.lastGcID == mw.gcID && mw.visited[object.Addr] {
-		object.cyclicallyReferenced = true
-		return true
-	} else {
-		object.lastGcID = mw.gcID
-		mw.visited[object.Addr] = true
-		return false
-	}
-}
-
-func (mw markWorker) extractNestedObjects(object reflect.Value) (nestedObjects []reflect.Value) {
-	switch object.Kind() {
-	case reflect.Pointer, reflect.Interface:
-		nestedObjects = []reflect.Value{object.Elem()}
-	case reflect.Array, reflect.Slice:
-		for i := 0; i < object.Len(); i++ {
-			nestedObjects = append(nestedObjects, object.Index(i))
-		}
-	case reflect.Map:
-		mapIter := object.MapRange()
-		for mapIter.Next() {
-			nestedObjects = append(nestedObjects, mapIter.Key(), mapIter.Value())
-		}
-	case reflect.Struct:
-		for i := 0; i < object.NumField(); i++ {
-			nestedObjects = append(nestedObjects, object.Field(i))
-		}
-	}
-	return
-}
-
-func (mw markWorker) analyzeObject(metadata *objectMetadata) (nextObjects []*objectMetadata) {
-	object := reflect.NewAt(metadata.typeInfo, metadata.Addr).Elem()
-	nestedObjects := mw.extractNestedObjects(object)
-	for _, nestedObject := range nestedObjects {
-		addr := nestedObject.Addr().UnsafePointer()
-		if nestedMetadata, exist := mw.searchMetadata(addr); exist {
-			nextObjects = append(nextObjects, nestedMetadata)
-		} else {
-			// I can't trace it ¯\_(ツ)_/¯
-		}
-	}
-
-	return
-}
-
-func (mw markWorker) processObject(object *objectMetadata) {
-	skip := mw.markObject(object)
-	if !skip {
-		for _, nextObject := range mw.analyzeObject(object) {
-			mw.processObject(nextObject)
-		}
-	}
-
-	return
-}
-
-func (mw markWorker) mark(objects <-chan *objectMetadata) {
-	for object := range objects {
-		mw.processObject(object)
-	}
 }
 
 func (gen *Generation) Mark(searchMetadata func(unsafe.Pointer) (*objectMetadata, bool)) {
