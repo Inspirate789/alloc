3b692f565a3775601e49148512fd04b227709714
Inspirate789 <andreysapozhkov535@gmail.com>
2024-04-07 14:10:46 +0300

    Complete init function

--- init.go
+++ init.go
@@ -1,13 +1,30 @@
 package alloc
 
 import (
+	"context"
 	"github.com/Inspirate789/alloc/internal/generation"
+)
+
+const (
+	arenaSignalsQueueLen = 10
+	startHeapSize        = 4
+	startHeapTarget      = 15
 )
 
 var mainHypervisor *hypervisor
 
-func init() { // TODO: init GC state, channel and debugger.arenasAllocated
+func init() {
+	ctx, cancel := context.WithCancel(context.Background())
+	arenaSignals := make(chan struct{}, arenaSignalsQueueLen)
 	mainHypervisor = &hypervisor{
+		gcCtx:        ctx,
+		gcCancel:     cancel,
+		arenaSignals: arenaSignals,
+		gcState: gcState{
+			lastCollectedGenerations: -1,
+			curHeapSize:              startHeapSize,
+			heapTarget:               startHeapTarget,
+		},
 		mem: memory{
 			movingGenerations: []*generation.Generation{
 				generation.NewGeneration(), // young
@@ -17,6 +34,7 @@ func init() { // TODO: init GC state, channel and debugger.arenasAllocated
 			largeObjectGeneration: generation.NewGeneration(),
 		},
 	}
+	Debugger.arenasAllocated.Store(3)
 
 	go mainHypervisor.run()
 }
